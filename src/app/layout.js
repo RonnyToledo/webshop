bf157c23463939d757da510f03f85f2c92dc2beb
Ronny <ronnytoledo87@gmail.com>
2024-08-04 00:09:02 -0400

    com00:08-8/4

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ import { ThemeContext } from "@/context/createContext";
 import { Toaster } from "@/components/ui/toaster";
 import { Store } from "lucide-react";
 import Link from "next/link";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const context = createContext();
 const products = [];
@@ -61,6 +62,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider>
           <context.Provider value={{ store, dispatchStore }}>
             {children}
+            <SpeedInsights />
           </context.Provider>
         </ThemeProvider>
         <Toaster />
