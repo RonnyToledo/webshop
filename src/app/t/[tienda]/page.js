1087516f982bd973b29afeec444c6e726608378b
Ronny87 <ronnytoledo87@gmail.com>
2024-09-08 12:23:06 -0400

    Descripción de los cambios

--- src/app/t/[tienda]/page.js
+++ src/app/t/[tienda]/page.js
@@ -1,6 +1,5 @@
 import SHome from "@/components/Chadcn-components/sHome";
 import React from "react";
-import { createClient } from "@/lib/supabase";
 
 export default async function Page({ params }) {
   return (
