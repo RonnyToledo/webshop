5f056c5c451f286e0ae6e1d132610320e0335f1b
Ronny87 <ronnytoledo87@gmail.com>
2024-09-14 13:07:41 -0400

    Descripción de los cambios

--- src/components/Chadcn-components/Header.jsx
+++ src/components/Chadcn-components/Header.jsx
@@ -133,7 +133,7 @@ export default function Header({ tienda }) {
         >
           {pathname !== `/${store.variable}/${store.sitioweb}/search` ? (
             <div className="relative flex justify-between items-center border bg-white rounded-full w-full h-full p-2 grid-cols-4">
-              <Search className="absolute h-5 w-5 mr-3" />
+              <Search className="absolute h-5 w-5 mr-3 bg-white" />
               <span className="line-clamp-1 overflow-hidden w-full text-center">
                 {store.top}
               </span>
