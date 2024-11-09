43bf3c13d53be59334be393c99d6e2cbba0c89a8
Ronny87 <ronnytoledo87@gmail.com>
2024-11-09 18:57:28 -0500

    Descripción de los cambios

--- src/app/r/[tienda]/layout.js
+++ src/app/r/[tienda]/layout.js
@@ -59,23 +59,14 @@ export default function RootLayout({ children, params }) {
         </MyProvider>
 
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-gray-100 dark:bg-gray-800">
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            © {now.getFullYear()} R&H. All rights reserved.
-          </p>
-          <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-            <Link
-              className="text-xs hover:underline underline-offset-4 dark:text-gray-400 dark:hover:text-gray-50"
-              href="#"
-            >
-              Terms of Service
-            </Link>
-            <Link
-              className="text-xs hover:underline underline-offset-4 dark:text-gray-400 dark:hover:text-gray-50"
-              href="#"
-            >
-              Privacy
-            </Link>
-          </nav>
+          <Link
+            className="text-xs hover:underline underline-offset-4 dark:text-gray-400 dark:hover:text-gray-50"
+            href="/"
+          >
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              © {now.getFullYear()} R&H. All rights reserved.
+            </p>
+          </Link>
         </footer>
       </div>
     </main>
--- src/app/t/[tienda]/layout.js
+++ src/app/t/[tienda]/layout.js
@@ -77,23 +77,17 @@ export default function RootLayout({ children, params }) {
         </MyProvider>
 
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t bg-gray-100 dark:bg-gray-800">
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            © {now.getFullYear()} R&H. All rights reserved.
-          </p>
-          <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-            <Link
-              className="text-xs hover:underline underline-offset-4 dark:text-gray-400 dark:hover:text-gray-50"
-              href="#"
-            >
-              Terms of Service
-            </Link>
-            <Link
-              className="text-xs hover:underline underline-offset-4 dark:text-gray-400 dark:hover:text-gray-50"
-              href="#"
-            >
-              Privacy
-            </Link>
-          </nav>
+          <Link
+            className="w-full  text-xs hover:underline underline-offset-4 dark:text-gray-400 dark:hover:text-gray-50"
+            href="/"
+          >
+            <p className="flex flex-col justify-center align-center text-base w-fit text-gray-800 dark:text-gray-400">
+              {`R&H || Boulevard`}
+            </p>
+            <p className="flex flex-col justify-center align-center text-xs w-fit text-gray-500 dark:text-gray-400">
+              {`© ${now.getFullYear()} R&H. All rights reserved.`}
+            </p>
+          </Link>
         </footer>
       </div>
     </main>
